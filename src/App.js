0087c7e563afec4fd102479d7a94bc8d8670c1c8
ye6194 <ye6194@naver.com>
2024-03-03 01:35:22 +0900

    자바스크립트 전까지 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+function Main() {
+  return (
+    <main>
+      <p id="p1">안경을 벗고 싶다면?</p>
+      <p id="p2">안다와 함께하세요!</p>
+      <div><button><img id="heart" src="/pc_heart.png"></img>기대돼요</button></div>
+      <div id="expecting">{ }명이 기대하고 있어요!</div>
+    </main>
+  )
+}
+
+function Nav() {
+  return (
+    <nav>
+      <div id="menu"></div>
+      <img id="logo" src="/logo/logo.png" alt="logo"></img>
+    </nav>
+  )
+}
+
+function Img() {
+  return (
+    <img></img>
+    <img></img>
+    <img></img>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Nav></Nav>
+      <Main><Img></Img></Main>
+    </div >
   );
 }
 
