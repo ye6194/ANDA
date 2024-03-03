b5e6634d5f487f886f0d512555da9992187cd850
ye6194 <ye6194@naver.com>
2024-03-03 21:49:46 +0900

    캐러셀 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
 
-function Main() {
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
+function Slide(props) {
+
+  return (
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={props.images[0]}></img></SwiperSlide>
+        <SwiperSlide><img src={props.images[1]}></img></SwiperSlide>
+        <SwiperSlide><img src={props.images[2]}></img></SwiperSlide>
+        <SwiperSlide><img src={props.images[3]}></img></SwiperSlide>
+        <SwiperSlide><img src={props.images[4]}></img></SwiperSlide>
+        <SwiperSlide><img src={props.images[5]}></img></SwiperSlide>
+        <SwiperSlide><img src={props.images[6]}></img></SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+function Main({ children }) {
   const [isClicked, setIsClicked] = useState(false);
   const [expectingCount, setExpectingCount] = useState(0);
 
@@ -21,6 +52,7 @@ function Main() {
         </button>
       </div>
       <div id="expecting">{expectingCount}명이 기대하고 있어요!</div>
+      {children}
     </main>
   )
 }
@@ -45,10 +77,18 @@ function Img() {
 }
 
 function App() {
+  const images = [
+    "/swiper-slide/anda_swiper1.png", "/swiper-slide/anda_swiper2.png",
+    "/swiper-slide/anda_swiper3.png", "/swiper-slide/anda_swiper4.png",
+    "/swiper-slide/anda_swiper5.png", "/swiper-slide/anda_swiper6.png",
+    "/swiper-slide/anda_swiper7.png"
+  ]
+
   return (
     <div className="App">
       <Nav></Nav>
-      <Main><Img></Img></Main>
+      <Main><Slide images={images}></Slide></Main>
+
     </div >
   );
 }
