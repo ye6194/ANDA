fb8e844b260512891ba29b3b92634840b4a54bc6
ye6194 <ye6194@naver.com>
2024-03-03 17:01:56 +0900

    버튼 클릭 시 css, 기대하는 사람 수 바뀌게

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import { useState } from "react";
 
 function Main() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [expectingCount, setExpectingCount] = useState(0);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+    setExpectingCount(isClicked ? expectingCount - 1 : expectingCount + 1);
+  }
+
   return (
     <main>
       <p id="p1">안경을 벗고 싶다면?</p>
       <p id="p2">안다와 함께하세요!</p>
-      <div><button><img id="heart" src="/pc_heart.png"></img>기대돼요</button></div>
-      <div id="expecting">{ }명이 기대하고 있어요!</div>
+      <div>
+        <button className={isClicked ? "clicked" : ""} onClick={handleClick}>
+          <img id="heart" src={isClicked ? "/pc_heart_clicked.png" : "/pc_heart.png"}></img>기대돼요
+        </button>
+      </div>
+      <div id="expecting">{expectingCount}명이 기대하고 있어요!</div>
     </main>
   )
 }
@@ -24,9 +36,11 @@ function Nav() {
 
 function Img() {
   return (
-    <img></img>
-    <img></img>
-    <img></img>
+    <div>
+      <img></img>
+      <img></img>
+      <img></img>
+    </div>
   )
 }
 
